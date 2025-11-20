179c87d239352e8ac6ac2a35bcf5535d57b10c95
Amanda <amandapedroso@edu.unifil.br>
2025-11-20 15:43:58 -0300

    chore: arquivo(s) inicial(is)

---
+++ MergeNoTerminal.java
@@ -0,0 +1,19 @@
+public class MergeNoTerminal {
+
+        public static void main(String[] args) {
+
+            int numero1 = 10;
+            int numero2 = 5;
+
+            System.out.println("=== Atividade ===");
+            System.out.println("Número 1: " + numero1);
+            System.out.println("Número 2: " + numero2);
+
+            // ESTA É A LINHA QUE OS DOIS VÃO ALTERAR → GERAR CONFLITO
+            int resultado = 0; // TODO: João E Davi alterem aqui
+
+            System.out.println("Resultado: " + resultado);
+        }
+    }
+
+
